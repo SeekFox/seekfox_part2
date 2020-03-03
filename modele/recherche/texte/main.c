2dbae03ea9241d8b6febf44ad883501d49d17892
Clement Truillet <clem.truillet@gmail.com>
2020-03-03 09:01:38 +0100

    Initialisation de rechercheTexte.c

--- modele/recherche/texte/main.c
+++ modele/recherche/texte/main.c
@@ -11,6 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "texte.h"
 #include "../config.h"
 
@@ -22,17 +23,53 @@ Config config;
 
 int main (int argc, char * argv[]){
 
-    printf("RECHERCHE TEXTE\n\n");
+    //Initialisation
+    FILE * output = NULL;
+    output = fopen("rechercheTexteOut.txt","w+");
+
+    //Le fichier de sortie est ouvert ?
+    if(output==NULL){
+        printf("ERREUR : \'rechercheTexteOut.txt\' dosen\'t exist.\n");
+        return 1;
+    }
+
+    //Verification nombre de parametres
+    if(argc!=2){
+        fprintf(output,"ERREUR : \'%s\' expected one (1) argument.\n", argv[0]);
+        return 2;
+    }
+
+    FILE * fichier = NULL;
+    char * cheminFichier = (char*)malloc(sizeof(char)*strlen(argv[1]));
+
+    strcpy(cheminFichier,argv[1]);
+    fichier = fopen(cheminFichier,"r");
+
+    //Fichier exite ?
+    if(fichier==NULL){
+        fprintf(output,"ERREUR : \'%s\' can\'t be read or doesn\'t exist.\n", cheminFichier);
+        return 3;
+    }
+
+    if(strcmp(getExtensionOfFile(cheminFichier),".xml")!=0){
+        fprintf(output,"ERREUR : \'%s\' isn\'t supported by \'%s\'.\n",cheminFichier,argv[0]);
+        return 4;
+    }
+
+    fprintf(output,"Extension : %s\n",getExtensionOfFile(cheminFichier));
 
     
     //recuperation des parametres de config
     config = loadConfig();
 
-    for(int i = 0; i<argc; i++){
-        printf("%s\n",argv[i]);
-    }
+    
+    //Execution
+    DescripteurTexte dt = initDescripteurTexte();
+    dt = lireFichierTexte(cheminFichier);
+    afficheDescripteurTexte(dt);
 
-    printf("CONFIG %s",getLogicielTexte(config));
+
+    //fprintf(output,"CONFIG %s",getLogicielTexte(config));
 
 
     return 0;
