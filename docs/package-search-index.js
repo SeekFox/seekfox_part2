f5b0b26eee1f060ce0dc548a04f4324a4267a37f
ctruillet <clem.truillet@gmail.com>
2020-04-30 16:32:46 +0200

    :pencil: Javadoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,6 @@
+/*
+ * Copyright (c) 2020.
+ * Clément Truillet (clement@ctruillet.eu)
+ */
+
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"controleur"},{"l":"modele"},{"l":"vue"},{"l":"vue.FileChooser"}]
